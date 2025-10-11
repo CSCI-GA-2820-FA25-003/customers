61d11338faa0a2e0c97e8868cae67f962331ef99
soham-chitnis10 <sohamchitnis10@gmail.com>
2025-10-11 01:07:08 +0000

    added root URL listing customers

--- service/routes.py
+++ service/routes.py
@@ -38,7 +38,7 @@ def index():
         jsonify(
             name="Customers Demo REST API Service",
             version="1.0",
-            # paths=url_for("update_customers", _external=True),
+            paths=url_for("list_customers", _external=True),
         ),
         status.HTTP_200_OK,
     )
